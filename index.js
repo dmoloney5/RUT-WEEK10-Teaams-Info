80cc7a150ba033fa7b04a7c8e95204a7937e48a6
techman <dmoloney5@gmail.com>
2022-03-05 12:23:05 -0500

    finished all lib

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //adding Node Module
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require("path");
 
 // adding Employees
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 //make HTML file
 const render = require("./lib/htmlRenderer");
